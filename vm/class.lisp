94ffa97ce387d7a36d392938680cea40326d902d
Bike <aeshtaer@gmail.com>
2022-10-19 22:39:45 -0400

    VM: Print classes more nicely

--- vm/class.lisp
+++ vm/class.lisp
@@ -8,15 +8,22 @@
   (:argument-precedence-order class object index new))
 
 (defclass proxy-class (class)
-  ((%constructor :initarg :constructor :reader constructor :type function)
+  ((%name :initarg :name :reader i:name :type symbol) ; debugging
+   (%constructor :initarg :constructor :reader constructor :type function)
    (%nslots :initarg :nslots :reader nslots :type (integer 0))
    (%tester :initarg :tester :reader tester :type function)
    (%reader :initarg :reader :reader reader :type function)
    (%writer :initarg :writer :reader writer :type function)))
 
-(defun make-proxy (constructor nargs tester reader writer)
+(defmethod print-object ((o proxy-class) s)
+  (print-unreadable-object (o s :type t)
+    (when (slot-boundp o '%name)
+      (write (i:name o) :stream s)))
+  o)
+
+(defun make-proxy (name constructor nargs tester reader writer)
   (make-instance 'proxy-class
-    :constructor constructor :nslots nargs
+    :name name :constructor constructor :nslots nargs
     :tester tester :reader reader :writer writer))
 
 (defmethod construct ((class proxy-class) &rest arguments)
@@ -42,29 +49,29 @@
   (declare (ignore new index))
   (error "Can't write a slot of this object: ~s" object))
 
-(defvar class:cell (make-proxy #'i:make-cell 1 #'i:cellp
-                               (lambda (cell index)
-                                 (declare (ignore index))
-                                 (i:cell-value cell))
-                               (lambda (new cell index)
-                                 (declare (ignore index))
-                                 (setf (i:cell-value cell) new))))
-(defvar class:cons (make-proxy #'cons 2 #'consp
-                               (lambda (cons index)
-                                 (ecase index
-                                   ((0) (car cons))
-                                   ((1) (cdr cons))))
-                               (lambda (new cons index)
-                                 (ecase index
-                                   ((0) (setf (car cons) new))
-                                   ((1) (setf (cdr cons) new))))))
-(defvar class:boolean (make-proxy #'illegal-construction 0
-                                  (lambda (o) (typep o 'boolean))
-                                  #'illegal-read #'illegal-write))
-(defvar class:null (make-proxy #'illegal-construction 0 #'null
-                               #'illegal-read #'illegal-write))
-(defvar class:applicative
-  (make-proxy #'i:wrap 1 (lambda (o) (typep o 'i:applicative))
+(defparameter class:cell (make-proxy 'class:cell #'i:make-cell 1 #'i:cellp
+                                     (lambda (cell index)
+                                       (declare (ignore index))
+                                       (i:cell-value cell))
+                                     (lambda (new cell index)
+                                       (declare (ignore index))
+                                       (setf (i:cell-value cell) new))))
+(defparameter class:cons (make-proxy 'class:cons #'cons 2 #'consp
+                                     (lambda (cons index)
+                                       (ecase index
+                                         ((0) (car cons))
+                                         ((1) (cdr cons))))
+                                     (lambda (new cons index)
+                                       (ecase index
+                                         ((0) (setf (car cons) new))
+                                         ((1) (setf (cdr cons) new))))))
+(defparameter class:boolean (make-proxy 'class:boolean #'illegal-construction 0
+                                        (lambda (o) (typep o 'boolean))
+                                        #'illegal-read #'illegal-write))
+(defparameter class:null (make-proxy 'class:null #'illegal-construction 0 #'null
+                                     #'illegal-read #'illegal-write))
+(defparameter class:applicative
+  (make-proxy 'class:applicative #'i:wrap 1 (lambda (o) (typep o 'i:applicative))
               (lambda (app index) (declare (ignore index)) (i:unwrap app))
               #'illegal-write))
 
